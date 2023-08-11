f713de61dd8cb6cbb9c4b265239a48496312cbaa
alex liao <alex640702070829@gmail.com>
2023-08-11 17:48:51 +0800

    :white_check_mark: Add level 26 DoubleEntryPoint solution

---
+++ src/levels/26-DoubleEntryPoint/DetectionBot.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.10;
+
+import './DoubleEntryPoint.sol';
+
+contract DetectionBot is IDetectionBot {
+    address immutable cryptoVault;
+
+    constructor(address _cryptoVault) {
+        cryptoVault = _cryptoVault;
+    }
+
+    function handleTransaction(address user, bytes calldata msgData) external {
+        // skip the first 4 bytes, because it's a function signatures
+        (,, address originSender) = abi.decode(msgData[4:], (address, uint256, address));
+
+        // prevent anyone from transferring the underlying token out of the vault
+        if(originSender == cryptoVault) {
+            IForta(msg.sender).raiseAlert(user);
+        }
+    }
+}
